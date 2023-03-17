49d108ce4fb7177225d3080092e7aa5c8c6b0ff1
Joaquin <joaquin.fumeaux@sofka.com.co>
2023-03-17 11:44:14 -0300

    ejemplo de mongoose para el servicio de productos

--- src/productos/productos.service.ts
+++ src/productos/productos.service.ts
@@ -7,35 +7,10 @@ import { Producto, ProductoDocument } from "./schema/producto.schema";
 
 @Injectable()
 export class ProductosService {
-  private productos: Producto[];
   constructor(
     @InjectModel(Producto.name)
     private productoModule: Model<ProductoDocument>
-  ) {
-    // this.productos = [
-    //   {
-    //     id: 1,
-    //     nombre: 'TFC ',
-    //     stock: 10,
-    //     precio: 500,
-    //     image: './img/tfc1.jpg',
-    //   },
-    //   {
-    //     id: 2,
-    //     nombre: 'TFC 2',
-    //     stock: 10,
-    //     precio: 400,
-    //     image: './img/tfc1.jpg',
-    //   },
-    //   {
-    //     id: 3,
-    //     nombre: 'TFC 3 ',
-    //     stock: 10,
-    //     precio: 450,
-    //     image: './img/tfc1.jpg',
-    //   },
-    // ];
-  }
+  ) {}
   create(createProductoDto: CreateProductoDto) {
     return this.productoModule.create(createProductoDto);
   }
@@ -49,12 +24,9 @@ export class ProductosService {
   }
 
   update(id: number, updateProductoDto: UpdateProductoDto) {
-    this.productoModule.findById(id);
-    return this.productoModule.create(updateProductoDto);
+    return this.productoModule.findByIdAndUpdate(id, updateProductoDto);
   }
   remove(id: number) {
-    const eliminar = this.productos.find((prod) => prod.id === id);
-    this.productos = this.productos.filter((prod) => prod.id !== id);
-    return eliminar;
+    return this.productoModule.findByIdAndDelete(id);
   }
 }
